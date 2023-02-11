60fd4e94fd1e7f0a85794da4c5229de2e53957aa
Aleksey <aliaksei.balabushka@gmail.com>
2023-02-11 14:41:45 +0300

    fix: add auth reduces in combine reducers

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { appInterface } from "./appInterface";
+import { auth } from "./auth";
 
 const rootReducer = combineReducers({
   appInterface,
+  auth,
 });
 
 export default rootReducer;
