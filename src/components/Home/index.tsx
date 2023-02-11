ba7f69224363ee6e122d61c9069ea6a5e735b41f
Aleksey <aliaksei.balabushka@gmail.com>
2023-02-11 14:50:37 +0300

    feat: add links in home page

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,7 +1,22 @@
 import "./home.scss";
 import { FunctionComponent } from "react";
+import { Link } from "react-router-dom";
 
 const Home: FunctionComponent = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1>Home</h1>
+      <div>
+        <Link className="text-link" to="/dashboard">
+          Open DASHBOARD (temporary link)
+        </Link>
+      </div>
+      <div>
+        <Link className="text-link" to="/sign-in">
+          Open SIGN-IN (temporary link)
+        </Link>
+      </div>
+    </div>
+  );
 };
 export default Home;
