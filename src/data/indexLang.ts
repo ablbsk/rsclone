e5a10651aca488bcb7a615614d018b447ef66ecb
anast-ananko <anast.ananko@gmail.com>
2023-02-15 01:56:08 +0200

    feat: add lang for profile

--- src/data/indexLang.ts
+++
@@ -1,4 +0,0 @@
-export { default as carouselLang } from "./carousel";
-export { default as dashboardLang } from "./dashboard";
-export { default as infoSection } from "./infoSection";
-export { default as plainsLang } from "./plaints";
